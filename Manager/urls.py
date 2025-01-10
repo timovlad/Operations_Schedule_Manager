4f17417d9968f15af79286d44223abd036b05a83
Vlad Tymoshko <slradbez@gmail.com>
2025-01-10 12:58:57 +0100

    create html and update urls

--- Manager/urls.py
+++ Manager/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('patients/new/', views.patient_create, name='patient_create'),
     path('login/', auth_views.LoginView.as_view(template_name='Manager/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+path('patients/edit/<int:pk>/', views.patient_update, name='patient_update'),
+path('patients/delete/<int:pk>/', views.patient_delete, name='patient_delete'),
+
 ]
