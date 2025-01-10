b0ae3b17d250500078811cdf0156c2a438228ec9
Vlad Tymoshko <slradbez@gmail.com>
2025-01-10 21:17:21 +0100

    update forms for time

--- Manager/forms.py
+++ Manager/forms.py
@@ -1,5 +1,6 @@
-from django import forms
 from .models import Patient, Surgery
+from django import forms
+from django.forms.widgets import DateTimeInput
 
 
 class PatientForm(forms.ModelForm):
@@ -9,6 +10,16 @@ class PatientForm(forms.ModelForm):
 
 
 class SurgeryForm(forms.ModelForm):
+    start_time = forms.DateTimeField(
+        label="Дата и время операции",
+        input_formats=["%d.%m.%Y %H:%M", "%Y-%m-%d %H:%M"],
+        widget=DateTimeInput(attrs={
+            "class": "form-control",
+            "placeholder": "дд.мм.гггг чч:мм",
+        }),
+        help_text="Введите дату и время операции в формате дд.мм.гггг чч:мм"
+    )
+
     class Meta:
         model = Surgery
         fields = ["start_time", "surgery_name", "operating_room"]
