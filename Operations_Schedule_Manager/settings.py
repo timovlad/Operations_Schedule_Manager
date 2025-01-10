669becf04e9e43238b14e2b3194d0d9904540981
Vlad Tymoshko <slradbez@gmail.com>
2025-01-10 11:33:12 +0100

    Update html file and bootstrap include

--- Operations_Schedule_Manager/settings.py
+++ Operations_Schedule_Manager/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
